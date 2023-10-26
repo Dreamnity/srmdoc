9ac71d227132989b660b2cc623c3cdca850436ba
5UP3R_D1N <superdinmc@gmail.com>
2023-10-26 17:05:19 +0000

    forgor

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Dreamnity',
-  tagline: "Dreamnity's largest minecraft network",
+  tagline: "Dreamnity's minecraft network",
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
